4a24508588794bec4f7fc9af0963e28d24660579
Kumar_Timalsina <xmp19@txstate.edu>
2026-03-02 19:49:54 -0600

    Load ResNet50 with pre-trained ImageNet weights

--- inference-engine/model.py
+++ inference-engine/model.py
@@ -5,4 +5,8 @@ from torchvision import models
 
 class DRClassifier(nn.Module):
     """ResNet50 backbone with a custom FC head for DR grading."""
- 
+    def __init__(self, num_classes=5, freeze_backbone=True):
+        super(DRClassifier, self).__init__()
+
+        #Load ResNet50 with pre-trained ImageNet weights
+        self.backbone = models.resnet50(weights='IMAGENET1K_V1')
