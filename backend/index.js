6a87eb3fb908c60bbfee9226e6a2b081c07bb227
Kumar_Timalsina <xmp19@txstate.edu>
2026-03-26 08:06:07 -0500

    Register API route

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const scanRoutes = require('./routes/scan');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/scan', scanRoutes);
 
 // Basic Route
 app.get('/', (req, res) => {
